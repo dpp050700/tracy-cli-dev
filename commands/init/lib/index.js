d4cc03ee1671edcfdb3f2f1c021ee62d3c26274c
戴鹏鹏 <pengpeng.dai@china.zhaogang.com>
2022-03-30 23:34:02 +0800

    fix: commit

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,5 +1,12 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const fse = require('fs-extra');
+
+const log = require('@tracy-cli-dev/log');
 const Command = require('@tracy-cli-dev/command');
-const log = require('npmlog');
+
+const TYPE_PROJECT = 'project';
+const TYPE_COMPONENT = 'component';
 
 class InitCommand extends Command {
   init() {
@@ -10,13 +17,92 @@ class InitCommand extends Command {
     log.verbose(`input force: ${this.force}`);
   }
 
-  exec() {
-    // console.log(this.force);
+  isDirEmpty(dirPath) {
+    let fileList = fs.readdirSync(dirPath);
+    fileList = fileList.filter((file) => (
+      !file.startsWith('.') && ['node_modules'].indexOf(file) < 0
+    ));
+    return fileList.length === 0;
+  }
+
+  async getProjectInfo() {
+    let projectInfo = {};
+
+    const { type } = await inquirer.prompt({
+      type: 'list',
+      name: 'type',
+      message: '请选择初始化类型',
+      default: TYPE_PROJECT,
+      choices: [
+        { name: '项目', value: TYPE_PROJECT },
+        { name: '组件', value: TYPE_COMPONENT },
+      ],
+    });
+    log.verbose(type);
+    if (type === TYPE_PROJECT) {
+      projectInfo = await inquirer.prompt(
+        [
+          {
+            type: 'input', name: 'projectName', message: '请输入项目的名称', validate() { return true; },
+          },
+          {
+            type: 'input', name: 'projectVersion', message: '请输入项目的版本号', validate() { return true; },
+          },
+        ],
+      );
+    } else {
+      projectInfo = {};
+    }
+    console.log(projectInfo);
+    return projectInfo;
+  }
+
+  async prepare() {
+    const localPath = process.cwd();
+    // 判断当前目录是否为空
+    if (!this.isDirEmpty(localPath)) {
+      if (!this.force) {
+        const { ifContinue } = await inquirer.prompt([
+          {
+            type: 'confirm',
+            name: 'ifContinue',
+            message: '当前文件夹不为空，是否继续创建项目？',
+            default: false,
+          },
+          /* Pass your questions in here */
+        ]);
+        if (!ifContinue) {
+          return;
+        }
+      }
+
+      const { confirmDelete } = await inquirer.prompt([
+        {
+          type: 'confirm',
+          name: 'confirmDelete',
+          message: '继续创建将清空当前文件夹下所有内容，是否确认？',
+          default: false,
+        },
+      ]);
+
+      if (confirmDelete) {
+        fse.emptyDirSync(localPath);
+      }
+    }
+    Promise.resolve(this.getProjectInfo());
+    // return this.getProjectInfo();
+  }
+
+  async exec() {
+    try {
+      await this.prepare();
+    } catch (error) {
+      log.error(error.message);
+    }
   }
 }
 
 function init(...argv) {
-  // console.log(projectName, cmdObj, process.env.CLI_TARGET_PATH);
   const initCommand = new InitCommand(argv);
   return initCommand;
 }
